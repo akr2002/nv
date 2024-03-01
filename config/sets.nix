2c58659adefb96916c06e46981eafcd82a676237
Aditya <bluenerd@protonmail.com>
2024-03-01 15:05:51 +0530

    add neovide config

--- config/sets.nix
+++ config/sets.nix
@@ -85,9 +85,51 @@
         "i-ci-ve:ver25" # Insert, command-line insert, visual-exclude: vertical bar with 25% width
         "r-cr:hor20" # Replace, command-line replace: horizontal bar cursor with 20% height
         "o:hor50" # Operator-pending: horizontal bar cursor with 50% height
-        "a:blinkwait700-blinkoff400-blinkon-250-Cursor/lCursor" # All mdoes: blinking settings
+        "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor" # All mdoes: blinking settings
         "sm:block-blinkwait175-blonkoff150-blinkon175" # Stopwatch: block cursor with specific blink settings
       ];
+
+      # Chars list
+      list = true; # Show invisible characters
+      listchars = "eol:↲,tab:|->,lead:·,space: ,trail:•,extends:→,precedes:←,nbsp:␣";
+      
+      # Space in neovim command line to display messages
+      cmdheight = 2;
+
+      # Showmode
+      showmode = true; # TODO change to false
+
+      # Maximum number of items to show in popup menu (0 - use available screen space)
+      pumheight = 0;
+
+      # Format lines if supported by language server
+      formatexpr = "v:lua.require'conform'.formatexpr()";
+
+      laststatus = 3;
     };
+
+    extraConfigLua = ''
+      local opt = vim.opt
+      local g = vim.g
+      local o = vim.o
+
+      -- Neovide
+      if g.neovide then
+        -- Neovide options
+        g.neovide_fullscreen = false
+        g.neovide_hide_mouse_when_typing = false
+        g.neovide_refresh_rate = 60
+        g.neovide_cursor_vfx_mode = "ripple"
+        g.neovide_cursor_animkate_command_line = true
+        g.neovide_cursor_animate_in_insert_mode = true
+        g.neovide_cursor_vfx_particle_lifetime = 5.0
+        g.neovide_cursor_particle_density = 14.0
+        g.neovide_cursor_vfx_particle_speed = 12.0
+        g.neovide_transparency = 0.8
+
+        -- Neovide Fonts
+        o.guifont = "JetBrainsMono Nerd Font"
+      end
+    '';
   };
 }
